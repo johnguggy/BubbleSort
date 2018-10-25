b26eeab6fbdd7ea5da28f67a64dc90c28e535b4d
johnguggy <42472160+johnguggy@users.noreply.github.com>
2018-10-25 14:59:20 -0400

    Add files via upload

---
+++ BubbleSort/test3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[5] { 17, 12, 21, 9, 25 };
+            int[] Results = BubbleSort(nums);
+        }
+
+        static int[] BubbleSort(int[] numbers)
+        {
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    if (numbers[i] > numbers[i + 1])
+                    {
+                        int temp = numbers[i + 1];
+                        numbers[i + 1] = numbers[i];
+                        numbers[i] = temp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped == true);
+            return numbers;
+        }
+    }
+}
